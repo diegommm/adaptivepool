25993e7b198e86a64f9a878457bfed272cc56daa
Diego Augusto Molina <diegoaugustomolina@gmail.com>
2024-10-28 02:35:39 -0300

    Improve Stats benchmarks setup
    ```shell
    $ go test -run=- -bench=Stats -count=20 | benchstat -col=/implem -
    goos: linux
    goarch: amd64
    pkg: github.com/diegommm/adaptivepool
    cpu: 13th Gen Intel(R) Core(TM) i7-13700H
             │   default   │
             │   sec/op    │
    Stats-20   6.040n ± 4%
    
             │  default   │
             │    B/op    │
    Stats-20   0.000 ± 0%
    
             │  default   │
             │ allocs/op  │
    Stats-20   0.000 ± 0%
    ```

--- stats_bench_test.go
+++ stats_bench_test.go
@@ -5,29 +5,18 @@ import "testing"
 func BenchmarkStats(b *testing.B) {
 	// Consider running this benchmark like this for consistency with previous
 	// commits
-	//	go test -run=- -bench=Stats/implem -count=20 | benchstat -col=/implem -
+	//	go test -run=- -bench=Stats -count=20 | benchstat -col=/implem -
 
-	values := allTestDataInputValues(b)
-	b.Run("implem=default", benchStats(new(Stats), values))
-}
+	b.Run("implem=default", func(b *testing.B) {
+		st := new(Stats)
+		b.ReportAllocs()
+		b.ResetTimer()
 
-func benchStats(st stats, values []float64) func(b *testing.B) {
-	lower := func(a, b float64) float64 {
-		if a < b {
-			return a
-		}
-		return b
-	}
-	var witness float64 // prevent the compiler from being too smart
-
-	return func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			for _, v := range values {
-				st.Push(v)
-				witness = lower(witness, st.N())
-				witness = lower(witness, st.Mean())
-				witness = lower(witness, st.StdDev())
-			}
+			st.Push(float64(i))
+			st.N()
+			st.Mean()
+			st.StdDev()
 		}
-	}
+	})
 }
